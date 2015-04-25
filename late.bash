778e5bac431ae687afee8c6e2e073cc84b89d643
Bailan Li <bailanli@usc.edu>
2015-04-25 09:54:26 -0700

    add script for extracting committed date

---
+++ late.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+STUDENTS=$(ls -d *-Assignment1/)
+DIR=$PWD
+
+ASSIGNMENT=4a
+DIR=$PWD
+#TAG=a1p1
+#FOLDER=wordcount
+
+case "$ASSIGNMENT" in
+1)
+  TAG=a1handin
+  FOLDER=mapreduce
+  ;;
+2a)
+  TAG=a2ahandin
+  FOLDER=viewservice
+  ;;
+2b)
+  TAG=a2bhandin
+  FOLDER=pbservice
+  ;;
+3a)
+  TAG=a3ahandin
+  FOLDER=paxos
+  ;;
+3b)
+  TAG=a3bhandin
+  FOLDER=kvpaxos
+  ;;
+4a)
+  TAG=a4ahandin
+  FOLDER=shardmaster
+  ;;
+*)
+  ;;
+esac
+
+for STUDENT in $STUDENTS
+do
+  #echo "  "$student
+  cd $DIR/$STUDENT
+  git checkout --quiet master
+  git pull --quiet
+  git checkout --quiet tags/$TAG
+  DATE_TIME=`git log --tags=${TAG} -1 --format="%ci"`
+  CONVERT_DATE_TIME=`date -d"${DATE_TIME}" "+%D %T"`
+  git log --tags=$TAG -1 --format="${STUDENT:0:-13};${CONVERT_DATE_TIME};%d" 
+done
